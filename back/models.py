752fb690797badc73cdabbd7a8f0e544ebaa75be
Tomas Castellano <tomas@itaptec.com.ar>
2025-05-18 17:14:51 -0300

    feat: endpoints para buscar aulas

--- back/models.py
+++ back/models.py
@@ -10,12 +10,46 @@ class Aula(DATABASE.Model):
     codigo = DATABASE.Column(DATABASE.String(50), unique=True, nullable=False)
     tipo_banco = DATABASE.Column(DATABASE.String(50), nullable=False)
     tipo_pizarron = DATABASE.Column(DATABASE.String(50), nullable=False)
+    posicion_x = DATABASE.Column(DATABASE.Float, nullable=True)
+    posicion_y = DATABASE.Column(DATABASE.Float, nullable=True)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "codigo": self.codigo,
+            "tipo_pizarron": self.tipo_pizarron,
+            "tipo_banco": self.tipo_banco,
+            "posicion_x": self.posicion_x,
+            "posicion_y": self.posicion_y
+        }
 
     def __repr__(self):
         return f"<Aula {self.codigo}>"
     
-def get_aula_by_codigo(aula_codigo):
+def aula_get_by_codigo(aula_codigo) -> Aula:
     """Devuelve el aula con el ID especificado."""
     aula = DATABASE.session.query(Aula).filter_by(codigo=aula_codigo).first()
     return aula
+
+def aula_get_all() -> list[Aula]:
+    """Devuelve todas las aulas."""
+    return DATABASE.session.query(Aula).all()
+
+
+class Materia(DATABASE.Model):
+    __tablename__ = 'materias'
+
+    id = DATABASE.Column(DATABASE.Integer, primary_key=True)
+    codigo = DATABASE.Column(DATABASE.String(50), unique=True, nullable=False)
+    nombre = DATABASE.Column(DATABASE.String(150), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "codigo": self.codigo,
+            "nombre": self.nombre
+        }
+
+def materia_get_all() -> list[Materia]:
+    """Devuelve todas las materias."""
+    return DATABASE.session.query(Materia).all()
--- back/routes.py
+++ back/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify
 from logger import get_logger
 from config import Config
 from models import *
@@ -8,34 +8,56 @@ from flask_restx import Api, Resource
 api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
 
 api = Api(api_bp, title="API de Aulas", version="1.0", description="Swagger con Blueprint")
-ns = api.namespace("aulas", description="Operaciones relacionadas con aulas")
+ns_aulas = api.namespace("aulas", description="Operaciones relacionadas con aulas")
+ns_materias = api.namespace("materias", description="Operaciones relacionadas con las materias")
 
 logger = get_logger()
 
-@ns.route("/")
-class Root(Resource):
+@ns_aulas.route("/")
+class AulaGetTodas(Resource):
     def get(self):
-        # llamar a la db para obtener un dato.
-        return {'message': 'Hola'}
+        """
+        Devuelve todas las aulas
+        """
+        aulas = aula_get_all()
 
-@ns.route("/<string:codigo_aula>")
-class AulaResource(Resource):
-    def get(self, codigo_aula):
+        return jsonify([aula.to_dict() for aula in aulas])
+
+@ns_aulas.route("/<string:codigo_aula>")
+class AulaGetPorCodigoResource(Resource):
+    def get(self, codigo_aula: str):
         """
         Devuelve la información del aula con el número especificado.
         """
-        # Supongamos que tenés una función o modelo que busca el aula:
-        aula = get_aula_by_codigo(codigo_aula)
+        
+        aula = aula_get_by_codigo(codigo_aula)
+
         if aula is None:
             return {"error": "Aula no encontrada"}, 404
-        # Si el aula existe, devuelve sus detalles
-        aula_data = {
-            "codigo": aula.codigo,
-            "tipo_banco": aula.tipo_banco,
-            "tipo_pizarron": aula.tipo_pizarron,
-        }
-        return aula_data, 200
-        #return aula, 200
+        
+        return jsonify(aula.to_dict()), 200
+    
+
+@ns_materias.route("/")
+class MateriasGetTodas(Resource):
+    def get(self):
+        """
+        Devuelve todas las materias
+        """
+
+        materias = materia_get_all()
+
+        return jsonify([materia.to_dict() for materia in materias])
+
+@ns_materias.route("/<string:codigo_materia>")
+class MateriaHorariosGetPorCodigo(Resource):
+    def get(self):
+        """
+        Devuelve todos los horarios de la materia
+        """
+
+        
+    
 
 def register_routes(app):
     app.register_blueprint(api_bp)
