a3c07dfeefecc9b9b9ce171a1cf67c3d87fd0a87
Tomas Castellano <tomas@itaptec.com.ar>
2025-06-14 17:30:26 -0300

    Elimina archivos ignorados que estaban cacheados

--- front/src/js/utils.js
+++
@@ -1,7 +0,0 @@
-
-export const MAP_OPTIONS = {
-    key: 'mik_D7AoZMisz1lZvdg655373076c',
-    secret: 'mis_B5YmdmHNyrQ4AL8qZdRDYcIHsinq4A7HBy3O9j8RRlh85480fd3',
-    mapId: '683cfcf0ef06ad000bd9c615',
-//    mapViewMode: 'ThreeD', 
-  };
--- front/www/index.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map', {
-    crs: L.CRS.Simple,
-});
-
-var bounds = [[0, 0], [191, 284]];
-var image = L.imageOverlay('mapa.png', bounds).addTo(map);
-
-map.fitBounds(bounds);
