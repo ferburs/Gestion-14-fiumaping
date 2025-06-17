8d83358d83434b278689b5179a6d9c437c8e7c3b
Tomas Castellano <tomas@itaptec.com.ar>
2025-06-17 19:35:05 -0300

    fix build

--- front/vite.config.js
+++ front/vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
       input: {
         index: resolve(__dirname, 'index.html'),
         mapa: resolve(__dirname, 'mapa.html'),
-        admin: resolve(__dirname, 'admin.html'),
         buscador_por_aula: resolve(__dirname, 'buscador_por_aula.html'),
         buscardo_por_materia: resolve(__dirname, 'buscardo_por_materia.html'),
       }
