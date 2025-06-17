be6bdbe9f2f59a062a93873471c31cc9f626844d
Listeria monocytogenes <listeria@disroot.org>
2025-06-17 19:12:50 -0300

    muestro los horarios de la materia en una tabla

--- front/src/js/buscador_materia.js
+++ front/src/js/buscador_materia.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const selectMateria = document.getElementById('selectMateria');
   const btnBuscar = document.getElementById('btnBuscarMateria');
   const resultado = document.getElementById('resultadoMateria');
+  const tablaAulasContainer = document.getElementById('tablaAulasContainer');
   const materiaTitulo = document.getElementById('materiaTitulo');
-  const materiaCodigo = document.getElementById('materiaCodigo');
-  const materiaAulas = document.getElementById('materiaAulas');
+  const templateTablaHorarios = document.querySelector('#templateTablaHorarios');
 
   let materiasData = {};
 
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const option = document.createElement('option');
         option.value = materia.codigo;
         option.textContent = `${materia.nombre} (${materia.codigo})`;
-        materiasData[materia.codigo] = materia
+        materiasData[materia.codigo] = materia;
         selectMateria.appendChild(option);
       }
 
@@ -49,8 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
 
     const materia = materiasData[codigo];
-    materiaTitulo.textContent = materia.nombre;
-    materiaCodigo.textContent = `Código: ${materia.codigo}`;
+    materiaTitulo.textContent = `${materia.nombre} (${materia.codigo})`;
 
     fetch(getFullEndpoint('/api/v1/materias/' + codigo))
       .then(response => {
@@ -58,13 +57,35 @@ document.addEventListener('DOMContentLoaded', () => {
         return response.json();
       })
       .then(cursos => {
-        const aulasHtml = cursos.map(un_curso => {
-          const datos_aula = un_curso.aula;
+        const clone = templateTablaHorarios.content.cloneNode(true);
+        const td = clone.querySelectorAll('td');
+        const th = clone.querySelectorAll('th');
+        const DIAS = [];
 
-          return `Aula ${datos_aula.codigo}: ${un_curso.dia_semana} de ${un_curso.hora_inicio} a ${un_curso.hora_fin}`;
-        }).join('<br>');
+        for (let i = 1; i <= 7; i++) {
+          DIAS.push(th[i].innerText.toLowerCase());
+        }
 
-        materiaAulas.innerHTML = `<strong>Aulas donde se dicta:</strong><br>${aulasHtml}`;
+        for (const curso of cursos) {
+          const aula = `Aula ${curso.aula.codigo}`;
+          const col = DIAS.indexOf(curso.dia_semana.toLowerCase());
+          const hora_i = parseInt(curso.hora_inicio) - 8;
+          const hora_f = parseInt(curso.hora_fin) - 8;
+          for (let i = hora_i; i < hora_f; i++) {
+            const celda = td[i * 7 + col];
+            celda.style.backgroundColor = '#2196F3';
+            celda.style.color = 'white';
+            celda.textContent = aula;
+          }
+        }
+
+        const oldMaterias = document.querySelector('#materiaAulas');
+        if (oldMaterias !== null) {
+          tablaAulasContainer.replaceChild(clone, oldMaterias);
+        } else {
+          tablaAulasContainer.appendChild(clone);
+        }
+
         resultado.classList.remove('d-none');
       });
   });
